cf377126d1e30783f8bcc421e7f4a38f1159e352
Elen Ismaylova <wonderelen@gmail.com>
2020-10-29 19:23:27 +0300

    commit done

---
+++ src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+                int transfer = 500_000_000;
+                int total = balance + transfer;
+        System.out.println(total);
+
+ }
+}
