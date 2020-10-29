0c08687b1eb71c9ac2f54f3bde0dd117567f43f7
Elen Ismaylova <wonderelen@gmail.com>
2020-10-29 22:05:16 +0300

    files added

--- src/main.java
+++ src/main.java
@@ -1,9 +1,9 @@
 
 public class main {
     public static void main(String[] args) {
-        int balance = 2000000000;
-                int transfer = 500_000_000;
-                int total = balance + transfer;
+        long balance = 2_000_000_000;
+         long transfer = 500_000_000;
+                long total = balance + transfer;
         System.out.println(total);
 
  }
